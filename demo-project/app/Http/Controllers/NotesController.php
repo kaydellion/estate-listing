1738fee6e0e61e409fbc22f92fc6239d80b6ee9b
kaydellion <kanyin500@gmail.com>
2025-01-08 23:26:06 +0100

    Notes feature

--- demo-project/app/Http/Controllers/NotesController.php
+++ demo-project/app/Http/Controllers/NotesController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notes;
 use Illuminate\Http\Request;
 
 class NotesController extends Controller
 {
-    //
+    //get single notes
+    function getUser($id){
+        $note= Notes::find($id);
+        return view('layouts.note',['note'=>$note]);
+        }
+    
+         //show all course
+       public function allCourses(){
+        $notes = Notes::all();  // Fetch all courses
+        return view('layouts.notes', ['courses' => $notes]);  // Pass collection as 'courses'
+        }
 }
