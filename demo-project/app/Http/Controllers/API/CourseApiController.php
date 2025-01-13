289cd160befca920aa2cdb1f704021d11882515b
kaydellion <kanyin500@gmail.com>
2025-01-13 22:09:13 +0100

    ApiResponse added

--- demo-project/app/Http/Controllers/API/CourseApiController.php
+++ demo-project/app/Http/Controllers/API/CourseApiController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+Use App\Models\Course;
 
 class CourseApiController extends Controller
 {
-    //
+    //GET, POST, PUT, DELETE (FETCH, ADD, UPDATE, DELETE)
 }
